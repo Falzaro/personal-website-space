3cdeeee72a5844d21861a51b548f41204617509f
Fitri Rozi <fitrirozi1@gmail.com>
2023-03-05 18:22:21 -0600

    ignored unknown errors

--- components/canvas/Stars.jsx
+++ components/canvas/Stars.jsx
@@ -7,9 +7,9 @@ import * as random from 'maath/random/dist/maath-random.esm';
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(5000), { radius: 1.2 })
-  );
+  const [sphere] = useState(() => {
+    return random.inSphere(new Float32Array(5000), { radius: 1.2 });
+  });
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
@@ -23,7 +23,7 @@ const Stars = (props) => {
           transparent
           color="#f272c8"
           size={0.002}
-          sizeAttenuation={true}
+          sizeAttenuation
           depthWrite={false}
         />
       </Points>
